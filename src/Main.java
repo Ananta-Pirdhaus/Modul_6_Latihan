b4d413561c95fef3eaa431688a3e99a5c7831cf4
anantapir <115287802+Ananta-Pirdhaus@users.noreply.github.com>
2023-06-12 10:35:16 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+    * @created ${DAY}/${MONTH}/${YEAR} - ${TIME}
+    * @project ${PROJECT_NAME}
+    * @author ${USER}
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
